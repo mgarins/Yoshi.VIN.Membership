93c9dfe6dedf7cd816d5b3412f1d5aeaba89188c
Martin Garins <marty@garins.com>
2017-08-21 19:45:57 -0500

    date formatting

--- Yoshi.VIN.Membership.Web.UI/ClientApp/app/components/membership/memberlist.component.ts
+++ Yoshi.VIN.Membership.Web.UI/ClientApp/app/components/membership/memberlist.component.ts
@@ -1,7 +1,8 @@
 ﻿import { Component, Inject, ViewEncapsulation, OnInit , EventEmitter, Input, Output} from '@angular/core';
 import { Http } from '@angular/http';
 import { Member } from '../../models/member';
-import  { DataTableModule }  from  "angular2-datatable";
+import { DataTableModule } from "angular2-datatable";
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'member-list',
